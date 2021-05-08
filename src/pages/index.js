fa39e01dd60a1d8771ca92dfb0c06142cdd5da0a
tunde ola <babatundeololade50@gmail.com>
2021-05-09 00:17:52 +0300

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <Link to="/contact/">Contact</Link>
+      <Header headerText="Hello Gatsby" />
+      <p>What a world.</p>
+      <img alt="good_one" src="https://images.unsplash.com/photo-1593642634443-44adaa06623a?ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1225&q=80" />
+    </div>
+  )
 }
